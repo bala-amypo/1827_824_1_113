41448d8834afc1b38fb7bb87f214d40a430a6369
bala-amypo <bala@amypo.in>
2025-12-17 09:27:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,31 +1,21 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.UniqueConstraint;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrePersist;
+
 
 
 @Entity
-@Table(
-    name = "student_profiles",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "studentId"),
-        @UniqueConstraint(columnNames = "email")
-    }
-)
+
 public class StudentProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(unquie = false)
     private String studentId;
 
     @Column(nullable = false)
