c972a5a8d5663d039b6f00ba2863c3516746a20c
bala-amypo <bala@amypo.in>
2025-12-17 09:17:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+
+
+@Entity
+@Table(
+    name = "student_profiles",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "studentId"),
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class StudentProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String studentId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String program;
+
+    private Integer yearLevel;
+
+    private Boolean isRepeatOffender = false;
+
+    private l createdAt;
+
+    @OneToMany(mappedBy = "studentProfile")
+    private List<IntegrityCase> integrityCases;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    
+}
