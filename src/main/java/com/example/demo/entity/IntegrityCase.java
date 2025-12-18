c23c236bd6abe2388e5734fdec2c1c1dbb3c962b
bala-amypo <bala@amypo.in>
2025-12-18 06:38:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -1,71 +1,78 @@
-package com.example.demo.entity;
 
 
-public class IntegrityCase{
-    private Long id;
-    private string coursecode;
-    private string instructorName;
-    private string description;
-    private Long yearLevel;
-    private string status;
+public class IntegrityCase {
+
+    private Long id;                     
+    private String courseCode;
+    private 
+    private String instructorName;
+    private String description;
+    private String status;               
     private LocalDate incidentDate;
     private LocalDateTime createdAt;
 
-     public Long getId(){
+    
+    public Long getId() {
         return id;
-     }
-     public void setId(Long id){
-        this.id=id;
-     }
-     public string getCoursecode(){
-        return coursecode;
-     }
-     public void setCoursecode(string coursecode){
-        this.coursecode=coursecode;
-     }
-     public string getInstructorName(){
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public StudentProfile getStudentProfile() {
+        return studentProfile;
+    }
+
+    public void setStudentProfile(StudentProfile studentProfile) {
+        this.studentProfile = studentProfile;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getInstructorName() {
         return instructorName;
-     }
-     public void setInstructorname(string instructorName){
-        this.instructorname=instuctorName;
-     }
-      public string getdescription(){
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getDescription() {
         return description;
-     }
-     public void setdescription(string descripition){
-        this.descripiton=descripition;
-     }
-     public string getstatus(){
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
         return status;
-     }
-      public void setstatus(string status){
-        this.status=status;
-     }
-     public Localdate getincidentDate(){
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getIncidentDate() {
         return incidentDate;
-     }
-      public void setincidentDate(string incidentDate){
-        this.incidentDate=incidentDate;
-     }
-     public LocalDateTime getcreatedAt(){
+    }
+
+    public void setIncidentDate(LocalDate incidentDate) {
+        this.incidentDate = incidentDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
         return createdAt;
-     }
-     public StudentProfile(Long id,string coursecode,string instructorName,string descripition,string status,string incidentDate,Boolean isRepeatOffender,LocalDateTime createdAt)
-{
-    this.id=id;
-    this.studentId=studentId;
-    this.name=name;
-    this.email=email;
-    this.program=program;
-    this.yearLevel=yearLevel;
-    this.isRepeatOffender=isRepeatOffender;
-    this.createdAt=createdAt;
-}
-public StudentProfile()
-{
+    }
 
-}
-
-
-
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
