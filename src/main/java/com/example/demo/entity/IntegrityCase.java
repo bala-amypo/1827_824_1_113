cba0df8cf083595e247531bcbd00c9cd51fb1946
bala-amypo <bala@amypo.in>
2025-12-18 06:28:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -1,56 +1,71 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
 
-// public class IntegrityCase{
-//     private Long id;
-//     private string coursecode;
-//     private string instructorName;
-//     private string description;
-//     private Long yearLevel;
-//     private string status;
-//     private LocalDate incidentDate;
-//     private LocalDateTime createdAt;
+public class IntegrityCase{
+    private Long id;
+    private string coursecode;
+    private string instructorName;
+    private string description;
+    private Long yearLevel;
+    private string status;
+    private LocalDate incidentDate;
+    private LocalDateTime createdAt;
 
-//      public Long getId(){
-//         return id;
-//      }
-//      public void setId(Long id){
-//         this.id=id;
-//      }
-//      public string getCoursecode(){
-//         return coursecode;
-//      }
-//      public void setCoursecode(string coursecode){
-//         this.coursecode=coursecode;
-//      }
-//      public string getInstructorName(){
-//         return instructorName;
-//      }
-//      public void setInstructorname(string instructorName){
-//         this.instructorname=instuctorName;
-//      }
-//       public string getdescription(){
-//         return description;
-//      }
-//      public void setdescription(string descripition){
-//         this.descripiton=descripition;
-//      }
-//      public string getstatus(){
-//         return status;
-//      }
-//       public void setstatus(string status){
-//         this.status=status;
-//      }
-//      public Localdate getincidentDate(){
-//         return incidentDate;
-//      }
-//       public void setincidentDate(string incidentDate){
-//         this.incidentDate=incidentDate;
-//      }
-//      public LocalDateTime getcreatedAt(){
-//         return createdAt;
-//      }
+     public Long getId(){
+        return id;
+     }
+     public void setId(Long id){
+        this.id=id;
+     }
+     public string getCoursecode(){
+        return coursecode;
+     }
+     public void setCoursecode(string coursecode){
+        this.coursecode=coursecode;
+     }
+     public string getInstructorName(){
+        return instructorName;
+     }
+     public void setInstructorname(string instructorName){
+        this.instructorname=instuctorName;
+     }
+      public string getdescription(){
+        return description;
+     }
+     public void setdescription(string descripition){
+        this.descripiton=descripition;
+     }
+     public string getstatus(){
+        return status;
+     }
+      public void setstatus(string status){
+        this.status=status;
+     }
+     public Localdate getincidentDate(){
+        return incidentDate;
+     }
+      public void setincidentDate(string incidentDate){
+        this.incidentDate=incidentDate;
+     }
+     public LocalDateTime getcreatedAt(){
+        return createdAt;
+     }
+     public StudentProfile(Long id,String studentId,String name,String email,String program,int yearLevel,Boolean isRepeatOffender,LocalDateTime createdAt)
+{
+    this.id=id;
+    this.studentId=studentId;
+    this.name=name;
+    this.email=email;
+    this.program=program;
+    this.yearLevel=yearLevel;
+    this.isRepeatOffender=isRepeatOffender;
+    this.createdAt=createdAt;
+}
+public StudentProfile()
+{
+
+}
 
 
 
-// }
+}
