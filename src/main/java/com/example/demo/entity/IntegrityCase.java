7465e8953d3f079549dc34b70094a232685f7cbb
bala-amypo <bala@amypo.in>
2025-12-18 05:23:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -3,7 +3,13 @@ package com.example.demo.entity;
 
 public class IntegrityCase{
     private Long id;
-    private QutaPlan studentProfile;
     private string coursecode;
+    private string instructorName;
+    private string description;
+    private Long yearLevel;
+    private string status;
+    private LocalDate incidentDate;
+    private LocalDateTime createdAt;
+    
 
 }
