d896642ecf951c18acaeb95b633bc2970365193f
bala-amypo <bala@amypo.in>
2025-12-22 09:17:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/RepeatOffenderRecordController.java
+++ src/main/java/com/example/demo/controller/RepeatOffenderRecordController.java
@@ -1,34 +1,34 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.entity.RepeatOffenderRecord;
-import com.example.demo.service.RepeatOffenderRecordService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import com.example.demo.entity.RepeatOffenderRecord;
+// import com.example.demo.service.RepeatOffenderRecordService;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@RequestMapping("/api/repeat-offenders")
-@Tag(name = "Repeat Offender Records")
-public class RepeatOffenderRecordController {
+// @RestController
+// @RequestMapping("/api/repeat-offenders")
+// @Tag(name = "Repeat Offender Records")
+// public class RepeatOffenderRecordController {
 
-    private final RepeatOffenderRecordService service;
+//     private final RepeatOffenderRecordService service;
 
-    public RepeatOffenderRecordController(RepeatOffenderRecordService service) {
-        this.service = service;
-    }
+//     public RepeatOffenderRecordController(RepeatOffenderRecordService service) {
+//         this.service = service;
+//     }
 
-    @PostMapping
-    public RepeatOffenderRecord save(@RequestBody RepeatOffenderRecord record) {
-        return service.save(record);
-    }
+//     @PostMapping
+//     public RepeatOffenderRecord save(@RequestBody RepeatOffenderRecord record) {
+//         return service.save(record);
+//     }
 
-    @GetMapping
-    public List<RepeatOffenderRecord> getAll() {
-        return service.findAll();
-    }
-}
+//     @GetMapping
+//     public List<RepeatOffenderRecord> getAll() {
+//         return service.findAll();
+//     }
+// }
