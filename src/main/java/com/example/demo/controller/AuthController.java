f6cee548de6e11fa73813695b8af2382e5a30d4a
bala-amypo <bala@amypo.in>
2025-12-29 06:39:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,49 +1,10 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.dto.JwtResponse;
-// import com.example.demo.dto.LoginRequest;
-// import com.example.demo.dto.RegisterRequest;
-// import com.example.demo.service.AuthService;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// @RequestMapping("/auth")
-// public class AuthController {
-
-//     private final AuthService authService;
-
-//     public AuthController(AuthService authService) {
-//         this.authService = authService;
-//     }
-
-//     @PostMapping("/register")
-//     public ResponseEntity<String> register(
-//             @RequestBody RegisterRequest registerRequest) {
-
-//         authService.register(registerRequest);
-//         return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
-//     }
-
-//     @PostMapping("/login")
-//     public ResponseEntity<JwtResponse> login(
-//             @RequestBody LoginRequest loginRequest) {
-
-//         JwtResponse response = authService.login(loginRequest);
-//         return ResponseEntity.ok(response);
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.dto.JwtResponse;
 import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.AuthService;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -62,17 +23,17 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(
-            @RequestBody RegisterRequest request) {
+            @RequestBody RegisterRequest registerRequest) {
 
-        authService.register(request);
-        return ResponseEntity.ok("User registered successfully");
+        authService.register(registerRequest);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
-            @RequestBody LoginRequest request) {
+            @RequestBody LoginRequest loginRequest) {
 
-        JwtResponse response = authService.login(request);
+        JwtResponse response = authService.login(loginRequest);
         return ResponseEntity.ok(response);
     }
 }
